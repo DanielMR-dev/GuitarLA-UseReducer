54938fee50700b4af2990564c620ccc4f230b779
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2024-09-27 15:49:49 -0500

    178. Trabajando con la accion de incrementar cantidad

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,6 @@ type HeaderProps = {
     cart: CartItem[]
     dispatch: Dispatch<CartActions>
     decreaseQuantity: (id: Guitar['id'] ) => void
-    increaseQuantity: (id: Guitar['id'] ) => void
     clearCart: () => void
     isEmpty: boolean
     cartTotal: number
@@ -16,7 +15,6 @@ export default function Header({
         cart, 
         dispatch, 
         decreaseQuantity, 
-        increaseQuantity, 
         clearCart
     } : HeaderProps ) {
 
@@ -80,7 +78,7 @@ export default function Header({
                                                         <button
                                                             type="button"
                                                             className="btn btn-dark"
-                                                            onClick={() => increaseQuantity(guitar.id)}
+                                                            onClick={() => dispatch({type: 'increase-quantity', payload: {id: guitar.id}})}
                                                         >
                                                             +
                                                         </button>
